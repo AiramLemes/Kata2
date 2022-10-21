71ff636341e4e24028c583493a64c8ebae7fd53f
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-10-21 13:04:20 +0100

    Versión 1 Kata2

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,45 @@
+
+package kata2;
+
+/**
+ *
+ * @author airam
+ */
+
+import java.util.HashMap;
+ import java.util.Iterator;
+ import java.util.Map;
+
+
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+
+        int[] data = {1, 3, 3, 4, 5, 6, 7, 9, 9, 10, 11, 11, 14, 14, 14};
+
+        HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+
+
+
+
+
+        for (int i = 0; i < data.length; i++) {
+
+            if (histogram.containsKey(data[i])) {
+                histogram.put(data[i], histogram.get(data[i])+1);
+            }
+
+            else {
+                histogram.put(data[i], 1);
+            }
+        }
+
+        for (Integer key : histogram.keySet()) {
+
+            System.out.println(key + " ==> " + histogram.get(key));
+
+        }
+
+    }
+}
